41faddc6778bae9b32b1220c09a1c6533b383b57
victor-stephen <victorstephen047@gmail.com>
2025-02-08 09:18:53 +0100

    modified Jokes component

--- jokes/src/components/Jokes.jsx
+++ jokes/src/components/Jokes.jsx
@@ -3,15 +3,15 @@ import { useState, useEffect } from "react";
 export default function Jokes({ jokeData }) {
   const [isShown, setIsShown] = useState(false);
 
-  function showPunchline() {
+  function toggleShown() {
     setIsShown((prevStatus) => !prevStatus);
   }
 
   return (
     <div>
       {jokeData.setup  && <h3>{jokeData.setup}</h3>}
-      {isShown && <p>{jokeData.punchline}</p>}
-      <button onClick={showPunchline}>{isShown ? "Hide punchline" : "Show punchline"}</button>
+      {isShown ? <p>{jokeData.punchline}</p> : null}
+      <button onClick={toggleShown}>{isShown ? "Hide" : "Show"} punchline</button>
       <hr />
     </div>
   );
