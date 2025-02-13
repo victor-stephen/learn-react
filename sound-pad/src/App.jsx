b82bf3ab425797e815b0ec08fd3b847ab4033a1b
victor-stephen <victorstephen047@gmail.com>
2025-02-13 04:26:15 +0100

    Replace derived state in child with prop-based state management

--- sound-pad/src/App.jsx
+++ sound-pad/src/App.jsx
@@ -1,24 +1,34 @@
-import { useState } from 'react'
-import padsData from "./assets/pads"
-import Pad from "./components/Pad"
+import { useState } from "react";
+import padsData from "./assets/pads";
+import Pad from "./components/Pad";
 
 function App() {
-  const [pads, setPads] = useState(padsData)
+  const [pads, setPads] = useState(padsData);
 
-  const buttonElements = pads.map(pad => (
-    <Pad key={pad.id} color={pad.color} on={pad.on} />
-  ))
+  function toggleSwitch(id) {
+    setPads((prevPads) =>
+      prevPads.map((pad) =>
+        pad.id === id ? { ...pad, on: !pad.on } : pad
+      )
+    );
+  }
 
-  
+  const buttonElements = pads.map((pad) => (
+    <Pad
+      key={pad.id}
+      id={pad.id}
+      color={pad.color}
+      on={pad.on}
+      toggle={toggleSwitch}
+    />
+  ));
 
   return (
     <main>
-      <div className='pad-container'>
-        {buttonElements}
-      </div>
-      <button className='all-off'>Turn All Off</button>
+      <div className="pad-container">{buttonElements}</div>
+      <button className="all-off">Turn All Off</button>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- sound-pad/src/components/Pad.jsx
+++ sound-pad/src/components/Pad.jsx
@@ -1,13 +1,10 @@
-import React from "react";
-
 export default function Pad(props) {
-  const [on, setOn] = React.useState(props.on);
 
   return (
     <button
       style={{ backgroundColor: props.color }}
-      className={on ? "on" : undefined}
-      onClick={() => props.toggle(props.id)}
+      className={props.on ? "on" : undefined}
+      onClick={()=>props.toggle(props.id)}
     ></button>
   );
 }
